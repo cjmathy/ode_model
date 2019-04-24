666654387807b9d7f0cb587734b6be4d677d544a
Chris Mathy <cjmathy@gmail.com>
2019-04-23 23:34:44 -0700

    updating repo structure before debugging

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@
 
 from setuptools import setup
 
-setup(name='ode_modeler',
-      version='0.3',
-      description='ODE Model for Molecular Species',
-      author='Chris Mathy',
-      author_email='chris.mathy@ucsf.edu',
-      url='https://github.com/cjmathy/ode_modeler',
-      packages=['ode_modeler'],
-      )
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ode_modeler',
+    version='0.3',
+    description='ODE Model for Molecular Species',
+    long_description=readme,
+    author='Chris Mathy',
+    author_email='chris.mathy@ucsf.edu',
+    url='https://github.com/cjmathy/ode_modeler',
+    license=license,
+    packages=['ode_modeler']
+)
+
